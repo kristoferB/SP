6ed7f34cbd275da8825ecb761054f827389423dc
Daniel Nord <danielnord2@gmail.com>
2014-10-20 09:34:42 +0200

    Information about latest map version in relationIdent relationIdent layout change Google Font web dependency removed

--- gui/web/app/scripts/directives/relationident.js
+++ gui/web/app/scripts/directives/relationident.js
@@ -7,71 +7,90 @@
  * # relationident
  */
 angular.module('spGuiApp')
-  .directive('relationident', ['spTalker', 'notificationService', function (spTalker, notificationService) {
+  .directive('relationident', ['spTalker', 'notificationService', '$filter', function (spTalker, notificationService, $filter) {
     return {
       templateUrl: 'views/relationview.html',
       restrict: 'E',
       scope: {
         addWindow: '='
       },
-      link: function postLink(scope, element, attrs) {
-          scope.spTalker = spTalker;
-          scope.groupSelection = {};
-          scope.opSelection = {};
-          scope.things = {};
-          scope.initState = {};
-          scope.goalState = {};
+      link: function postLink(scope) {
+        scope.spTalker = spTalker;
+        scope.groupSelection = {};
+        scope.opSelection = {};
+        scope.things = {};
+        scope.initState = {};
+        scope.goalState = {};
+        scope.latestMapVersion = 0;
 
-          scope.findRelations = function(){
-
-            var operations = [], groups = [],
-              initState = [], goalState = [];
-
-            _.each(scope.opSelection, function(selected, id){
-              if(selected) operations.push(id);
-            });
-
-            _.each(scope.groupSelection, function(selected, group){
-              if(selected) groups.push(group);
-            });
-
-            console.log(scope.initState, scope.goalState);
-
-            _.each(scope.things, function(thing, id){
-              var initValue = false;
-              if(scope.initState[id] && scope.initState[id] !== '')
-                initValue = scope.initState[id];
-              else if (angular.isDefined(thing.attributes.stateVariable.init))
-                initValue = thing.attributes.stateVariable.init;
-              initState.push({id: id, value: initValue });
-
-              var goalValue = false;
-              if(scope.goalState[id] && scope.goalState[id] !== '')
-                goalValue = scope.goalState[id];
-              else if (!_.isUndefined(thing.attributes.stateVariable.goal))
-                goalValue = thing.attributes.stateVariable.goal;
-              goalState.push({id: id, goal: goalValue });
-            });
-
-            if(operations.length === 0) {
-              notificationService.info('You have to pick at least one operation.');
-              return
+        scope.getLatestMapVersion = function() {
+          var relationMaps = $filter('with')(spTalker.items, { isa: 'RelationResult' });
+          console.log(relationMaps);
+          _.each(relationMaps, function(relationMap){
+            if(relationMap.modelVersion > scope.latestMapVersion) {
+              scope.latestMapVersion = relationMap.modelVersion
             }
+          });
+        };
 
-            console.log(operations, initState, groups, goalState);
+        if(spTalker.itemsRead) {
+          scope.getLatestMapVersion();
+        }
 
-            var res = spTalker.findRelations(operations, initState, groups, goalState);
-            res.success(function (data) {
-              console.log(data);
-              scope.relations = data.relationmap.relationmap.map(function(item){
-                return item.sop
-              })
-            });
-            res.error(function(data) {
-              console.log(data);
-            });
+        scope.$on('itemsQueried', function() {
+          scope.getLatestMapVersion();
+        });
 
-          };
+        scope.findRelations = function(){
+
+          var operations = [], groups = [],
+            initState = [], goalState = [];
+
+          _.each(scope.opSelection, function(selected, id){
+            if(selected) operations.push(id);
+          });
+
+          _.each(scope.groupSelection, function(selected, group){
+            if(selected) groups.push(group);
+          });
+
+          console.log(scope.initState, scope.goalState);
+
+          _.each(scope.things, function(thing, id){
+            var initValue = false;
+            if(scope.initState[id] && scope.initState[id] !== '')
+              initValue = scope.initState[id];
+            else if (angular.isDefined(thing.attributes.stateVariable.init))
+              initValue = thing.attributes.stateVariable.init;
+            initState.push({id: id, value: initValue });
+
+            var goalValue = false;
+            if(scope.goalState[id] && scope.goalState[id] !== '')
+              goalValue = scope.goalState[id];
+            else if (!_.isUndefined(thing.attributes.stateVariable.goal))
+              goalValue = thing.attributes.stateVariable.goal;
+            goalState.push({id: id, goal: goalValue });
+          });
+
+          if(operations.length === 0) {
+            notificationService.info('You have to pick at least one operation.');
+            return
+          }
+
+          console.log(operations, initState, groups, goalState);
+
+          var res = spTalker.findRelations(operations, initState, groups, goalState);
+          res.success(function (data) {
+            console.log(data);
+            scope.relations = data.relationmap.relationmap.map(function(item){
+              return item.sop
+            })
+          });
+          res.error(function(data) {
+            console.log(data);
+          });
+
+        };
 
         scope.getSOP = function(){
           scope.sopError = "";
