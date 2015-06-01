b9478f6274cfe4ff61c9e401a610607a0d6b6bf6
Patrik <patrikm@chalmers.se>
2015-06-01 10:17:31 +0200

    SP Supremica
    Started to integrate SP with Supremica

---
+++ extensions/src/test/scala/sp/virtcom/SPSupremicaTest.scala
@@ -0,0 +1,16 @@
+package sp.virtcom
+
+import org.scalatest._
+
+/**
+ * Created by patrik on 2015-05-28.
+ */
+class SPSupremicaTest extends FreeSpec {
+
+  "when creating a model" - {
+    "the collectors should be implemented correctly" in {
+
+    }
+  }
+
+}
