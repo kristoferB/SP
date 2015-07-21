c289dacdb4115d848544114c4b1504c9a06f37ac
danielnord2 <danielnord2@gmail.com>
2015-07-21 14:43:52 +0200

    Enhanced the model list using Smart Table library Bumped angular-mocks to 1.4.2

--- gui/web/src/app/core/core.module.js
+++ gui/web/src/app/core/core.module.js
@@ -5,6 +5,6 @@
         .module('app.core', [
             'ngAnimate', 'ngSanitize',
             'blocks.exception', 'blocks.logger', 'blocks.router',
-            'ui.router'
+            'ui.router', 'smart-table'
         ]);
 })();
