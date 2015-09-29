a9657016b812357c0be0114ff5f9393f442fecc4
Patrik <patrikm@chalmers.se>
2015-09-29 11:31:51 +0200

    sp service: work in process

--- gui/web/src/app/spServices/spServices.directive.js
+++ gui/web/src/app/spServices/spServices.directive.js
@@ -17,7 +17,8 @@
             scope: {
                 attributes: '=',
                 structure: '=',
-                key: '='
+                key: '=',
+                domainToSelectFrom: "="
             },
             compile: compile,
           controller: spServicesFormController,
@@ -61,24 +62,36 @@
                 vm.isA = ""
             } else if (!_.isUndefined(x.ofType)){
 
+                //core>model
                 if (x.ofType == "Option[ID]" && vm.key == "model") {
                     vm.isA = "Option[ID]Model";
                     vm.attributes = _.isUndefined(x.default) ? vm.reloadModelID : x.default;
+                //core>includeIDAbles
                 } else if (x.ofType == "List[ID]" && vm.key == "includeIDAbles") {
                     vm.isA = "List[ID]includeIDAbles";
                     vm.attributes = _.isUndefined(x.default) ? vm.reloadSelectedItems : x.default;
+                //Boolean
                 } else if (x.ofType == "Boolean") {
                     vm.isA = "Boolean";
-                    vm.attributes = _.isUndefined(x.default) ? false : x.default
-                } else if (x.ofType == "List[ID]") {
-                    vm.isA = "List[ID]";
-                    vm.attributes = _.isUndefined(x.default) ? [] : x.default
-                } else{
+                    vm.attributes = _.isUndefined(x.default) ? false : x.default;
+                //String
+                } else if (x.ofType == "String") {
+                    vm.isA = "String";
+                    vm.attributes = _.isUndefined(x.default) ? "" : x.default;
+                    if(!_.isUndefined(x.domain) && x.domain.length > 0) {
+                        vm.isA += "WithDomain";
+                        vm.domainToSelectFrom = x.domain;
+                    }
+                //List[ID] and List[String]
+                } else if (x.ofType == "List[ID]" || x.ofType == "List[String]") {
+                    vm.isA = "List[T]";
+                    vm.attributes = _.isUndefined(x.default) ? [] : x.default;
+                //The rest
+                } else {
                     vm.isA = "KeyDef"; // för att testa
-                    vm.attributes = _.isUndefined(x.default) ? "" : x.default
+                    vm.attributes = _.isUndefined(x.default) ? "" : x.default;
                 }
 
-//                vm.attributes = _.isUndefined(x.default) ? "" : x.default
             } else if (_.isObject(x)){
                 vm.isA = "object";
                 vm.attributes = {};
