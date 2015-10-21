9ebb16cebacf3507bd41cebb1fbb68cb9cb6f8ff
danielnord2 <danielnord2@gmail.com>
2015-10-21 15:53:42 +0200

    Add widget and Create model buttons moved to top navbar. Route title bar removed.

--- gui/web/src/app/layout/sp-top-nav.directive.js
+++ gui/web/src/app/layout/sp-top-nav.directive.js
@@ -22,11 +22,26 @@
     }
 
     /* @ngInject */
-    TopNavController.$inject = ['modelService'];
+    TopNavController.$inject = ['modelService', 'dashboardService', '$state', '$modal'];
 
-    function TopNavController(modelService) {
+    function TopNavController(modelService, dashboardService, $state, $modal) {
         var vm = this;
         vm.modelService = modelService;
+        vm.dashboardService = dashboardService;
+        vm.$state = $state;
+        vm.createModel = createModel;
+
+        function createModel() {
+            var modalInstance = $modal.open({
+                templateUrl: '/app/models/createmodel.html',
+                controller: 'CreateModelController',
+                controllerAs: 'vm'
+            });
+
+            modalInstance.result.then(function(chosenName) {
+                modelService.createModel(chosenName);
+            });
+        }
     }
 
 })();
