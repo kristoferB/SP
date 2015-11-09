4e86ad2d35ccb3957906ce3261cfc37eef488e46
Patrik Bergagard <patrikm@chalmers.se>
2015-11-09 09:46:12 +0100

    More on GKNcase.

--- extensions/src/main/scala/sp/virtcom/modeledCases/ROARcase.scala
+++ extensions/src/main/scala/sp/virtcom/modeledCases/ROARcase.scala
@@ -11,9 +11,25 @@ case class ROARcase(modelName: String = "ROAR - single bin") extends CollectorMo
 
   def hAtt(h: String) = SPAttributes("hierarchy" -> Set(h))
 
-  op("searchForBins", hAtt("Quad"))
-  op("dock", hAtt("Quad"))
-  op("calculatePaths", hAtt("PathPlanner"))
+  v(name = "vQuad", idleValue = "inDock", domain = Seq("searchingForBins"), attributes = hAtt("Quad"))
+  v(name = "vBinDetection_mockup", idleValue = "idle", attributes = hAtt("Quad"))
+  op("searchForBins", hAtt("Quad") merge
+    SPAttributes(aResourceTrans("vQuad", "inDock", "searchingForBins", "inDock")))
+  op("newBinDetected_mockup", hAtt("Quad") merge
+    SPAttributes(aResourceTrans("vBinDetection_mockup", "idle", "detecting", "idle")) merge
+    SPAttributes("preGuard" -> Set("vQuad == searchingForBins")) merge
+    SPAttributes("preAction" -> Set("vDetectedBins = atLeastOneBin"))
+  )
+
+  //  op("dock", hAtt("Quad"))
+  v(name = "vDetectedBins", idleValue = "non", domain = "atLeastOneBin", attributes = hAtt("PathPlanner"))
+//  v(name = "vPathPlanner", idleValue = "idle", attributes = hAtt("PathPlanner"))
+//  op("calculatePathToTruck", hAtt("PathPlanner") merge
+//    SPAttributes(aResourceTrans("vPathPlanner", "idle", "planning", "idle"))
+//  )
+//  op("calculatePathToBin", hAtt("PathPlanner") merge
+//    SPAttributes(aResourceTrans("vPathPlanner", "idle", "planning", "idle"))
+//  )
 
   //  domain = Seq("IdleAtTruck, GoingToBin, AtFullBin, PickingUpFullBin, GoingToTruckWithBin, At-
   //    TruckLift, BinPlaced,WaitingForBinToBeEmptied, PickingUpEmptyBin, PickedUpEmptyBin, ReturningEmp-
@@ -31,13 +47,17 @@ case class ROARcase(modelName: String = "ROAR - single bin") extends CollectorMo
     SPAttributes("preGuard" -> Set("roary_load == full")))
   op("placeBinAtTruckLift", hAtt("Roary") merge
     SPAttributes(aResourceTrans("roary_pos", "atTruckLift", "placingBinAtLift", "binPlaced")) merge
-    SPAttributes(aResourceTrans("roary_load", "full", "fullToNo", "no")))
+    SPAttributes(aCarrierTrans("roary_load", atStart = "full", atComplete = "no")) merge
+    SPAttributes(aCarrierTrans("vTruckLift_load", atStart = "no", atComplete = "bin"))
+  )
   op("backAwayFromLift", hAtt("Roary") merge
     SPAttributes(aResourceTrans("roary_pos", "binPlaced", "backingAway", "waitingForBinToBeEmptied")) merge
     SPAttributes("preGuard" -> Set("roary_load == no")))
   op("pickUpEmptyBin", hAtt("Roary") merge
     SPAttributes(aResourceTrans("roary_pos", "waitingForBinToBeEmptied", "pickingUpEmptyBin", "pickedUpEmptyBin")) merge
-    SPAttributes(aResourceTrans("roary_load", "no", "noToEmpty", "empty")))
+    SPAttributes(aResourceTrans("roary_load", "no", "noToEmpty", "empty")) merge
+    SPAttributes(aCarrierTrans("vTruckLift_load", atStart = "bin", atComplete = "no"))
+  )
   op("returnBin", hAtt("Roary") merge
     SPAttributes(aResourceTrans("roary_pos", "pickedUpEmptyBin", "returningEmptyBin", "atEmptyBin")) merge
     SPAttributes("preGuard" -> Set("roary_load == empty")))
@@ -48,8 +68,13 @@ case class ROARcase(modelName: String = "ROAR - single bin") extends CollectorMo
     SPAttributes(aResourceTrans("roary_pos", "atEmptyBin", "returningToTruckEmptyHanded", "idleAtTruck")) merge
     SPAttributes("preGuard" -> Set("roary_load == no")))
 
-  op("grippingBin", hAtt("TruckLift"))
-  op("emptyingBin", hAtt("TruckLift"))
-  op("releasingBin", hAtt("TruckLift"))
+  v(name = "vTruckLift_pos", idleValue = "atHome", attributes = hAtt("TruckLift"))
+  v(name = "vTruckLift_load", idleValue = "no", attributes = hAtt("TruckLift"))
+  op("emptyingBin", hAtt("TruckLift") merge
+    SPAttributes(aResourceTrans("vTruckLift_pos", "atHome", "emptying", "atHome")) merge
+    SPAttributes("preGuard" -> Set("vTruckLift_load == bin"))
+  )
+
+  x("truckLiftZone", "vTruckLift_pos == emptying && roary_pos == binPlaced")
 
 }
