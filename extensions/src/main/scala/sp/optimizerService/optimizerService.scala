589afe6e60c97f8b375d88e4023de76f570fad51
kristianarveeide <arve_eide@hotmail.com>
2016-03-22 14:34:14 +0100

    psudo code for Optimization algoritme

--- extensions/src/main/scala/sp/optimizerService/optimizerService.scala
+++ extensions/src/main/scala/sp/optimizerService/optimizerService.scala
@@ -9,40 +9,143 @@ import sp.extensions._
   * Created by Kristian Eide on 2016-03-09.
   */
 class optimizerService extends IDAble {
-
-  case class tempState (
-    name: String,
-    id: ID = id.newID(),
+/*
+  case class TempState (
+//    name: String,
     values: List(Contiodns) = List(),
-    fCost: Int) extends IDAble{
+    id: ID = id.newID()
+    ) extends IDAble{
   }
   case class Transition (
     gCost: Int,
     head: tempState,
     tail: tempState,
-    OP: tempOP,
-    id: ID = id.newID()) extends IDAble{
+    OPs: List(tempOP) = List(),
+    id: ID = id.newID()
+    ) extends IDAble{
   }
-  case class node (
-    name: String,
-    id: ID = id.newID(),
-    in: List(Transition) = List(),
+  case class Node (
+    Int: String,
+    state: tempState,
+    in: Transition,
     out: List(Transition) = List(),
-    hCost: Int)
-  extends IDAble {
+    gCost: Int,
+    hCost: Int,
+    fCost: int,
+    id: ID = id.newID()
+    ) extends IDAble {
   }
 
-  case class tempOP(
+  case class TempOP(
   condisitons: List(condition) = List(),
-  actions: List(abillity) = List())
-  extends IDAble{
+  actions: List(abillity) = List(),
+  gCost: Int,
+  id: ID = id.newID()
+  )  extends IDAble{
   }
 
-  var opLIST: List(tempOP) = List(
-  var runFlexlink: teamOP = new tempOP List(condition) = List((moveOut || moveIN), List(abillity)= List(flexlink.run = true)),
-  var loadBuildPallets: teamOP = new tempOP List(condition) = List((orderBuild && operatorReady), List(abillity)= List(operatorLoading = true)),
-  var loadBuildPallets: teamOP = new tempOP List(condition) = List((orderMaterial && operatorReady), List(abillity)= List(operatorLoading = true)),
-  var raiseGive: teamOP = new tempOP List(condition) = List((), List(abillity)= List()), //ingen abiility då detta sker automatiskt då det finns pallet vid stop
+  var opLIST: List(TempOP) = List(
+  var runFlexlink: TeamOP = new TempOP List(condition) = List((moveOut || moveIN), List(abillity)= List(flexlink.run = true)),
+  var loadBuildPallets: TeamOP = new TempOP List(condition) = List((orderBuild && operatorReady), List(abillity)= List(operatorLoading = true)),
+  var loadBuildPallets: TeamOP = new TempOP List(condition) = List((orderMaterial && operatorReady), List(abillity)= List(operatorLoading = true)),
+  var raiseGive: TeamOP = new TempOP List(condition) = List((), List(abillity)= List()), //ingen abiility då detta sker automatiskt då det finns pallet vid stop
 
   )
+  */
+  //psudokod för optimerings algoritme
+  /*
+  tempNode createInitalNode(wallSchem: array[int]){
+    var j: int = 0,
+    for(int i; i <wallSchem.size(); i++){
+      if(0 < i){
+        j = j + 5;
+      }
+    }                                                  //5 the time place a kub
+
+   var newState: state new State (
+    List (Conditions) = List(new Conditions),
+    ID),
+   var newNode: node = new Node (
+    val name: Int =0,
+    newTempState,
+    null,
+    null,
+    var gCost: int = 0,
+    var hCost: int = 15 + j,                           //total time for all processes
+    var fCost: int = tempNode.gCost + tempNode.hCost,
+    ID)
+   return newTempNode
+  }
+  Node getNodeWithTransitions(node: Node){
+    var tempState: tempState = node.state,
+    var transitions: List(Transitions) = List(),
+    var OPs: List(TempOP) = getTempOP(tempState),
+    var parallelOPs: List(TempOP) = getTempOP(tempState)
+      for(each OPs: currentOP){
+        if(currentOP.gCost > 0){
+          parallelOPs.add(currentOP)
+        }
+      }
+      var parallelTransition: Transtion = new Transition(
+       currentOP.gCost,
+       null,
+       node,
+       currentOP,
+       ID)
+      node.out.add(parallelTransition)
+      for(each OPs: currentOP){
+        if(currentOP.gCost == 0){
+          var transition: Transtion = new Transition(
+          currentOP.gCost,
+          null,
+          node,
+          currentOP,
+          ID)
+        node.out.add(transition)
+      }
+    }
+  }
+  SOP CreateSOP (initalNode: Node ){
+    var examinedNode: Node = initalNode,
+    var openNodeList: List(Node) = List(examinedNode),
+    while(true){
+      var closedNodeList: Liste(Node) List(examinedNode),
+      examinedNode = getNodeWithTransitions(examinedNode),
+      var transitions: List(transition)= examinedNode.out,
+      for (each transitions: currentT){
+          var newState: TempSstate = new State (
+          List (Conditions) = List(new Conditions),
+          ID),
+        var newNode: node = new Node (
+          val name: Int = openNodeList.getLast().name++,
+          newTempState,
+          currentT,
+          null,
+          var gCost :int = currentT.gCost + examinedNode.gCost,
+          var hCost: int = examinedNode.hCost - currentT.cost*currentT.ops.size(),
+          var fCost: int = examinedNode.gCost + examinedNode.hCost,
+          ID)
+        openNodeList.add(newNode)
+        )
+      }
+      for(each openNodeList: currentOpenNode){
+        if(currentNode != null && !openNodeList.contains(currentNode.name)){
+         examinedNode = currentNode
+          break,
+      }
+      for(each openNodeList: currentOpenNode){
+        if(currentOpenNode != null && !openNodeList.contains(currentOpenNode.name)){
+          if(currentNode.fCost < examinedNode.fCost
+          || currentOpenNode.fCost =< examinedNode.fCost && currentOpenNode.gCost =< examinedNode.gCost){
+            examinedNode = currentNode,
+          }
+        }
+      }
+      if(examinedNode.hCost == 0){
+        break                                           //terminate the while loop
+      }
+    }
+
+  }
+ */
 }
