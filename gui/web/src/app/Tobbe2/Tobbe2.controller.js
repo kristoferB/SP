c7e5823cc366684a54b0f7664fcdb79b40c84e9c
Ekras <Tobbe_live@hotmail.com>
2016-05-16 20:05:06 +0200

    added buttons

--- gui/web/src/app/Tobbe2/Tobbe2.controller.js
+++ gui/web/src/app/Tobbe2/Tobbe2.controller.js
@@ -18,9 +18,11 @@
         //functions
         vm.sendOrder = sendOrder;
         activate();
+        vm.myFunction = myFunction;
+        vm.activate2 = activate2;
 
         vm.value = 1;
-
+        vm.debug14 = 0;
 
         angular.module('staticSelect', [])
             .controller('Tobbe2Controller', ['$scope', function($scope) {
@@ -31,6 +33,34 @@
                 };
             }]);
 
+        /* When the user clicks on the button,
+         toggle between hiding and showing the dropdown content */
+        function myFunction() {
+            document.getElementById("myDropdown").classList.toggle("show");
+        }
+
+        // Close the dropdown menu if the user clicks outside of it
+        window.onclick = function(event) {
+            if (!event.target.matches('.dropbtn')) {
+
+                var dropdowns = document.getElementsByClassName("dropdown-content");
+                var i;
+                for (i = 0; i < dropdowns.length; i++) {
+                    var openDropdown = dropdowns[i];
+                    if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                    }
+                }
+            }
+        }
+
+
+        function activate2(int) {
+            if(int == 1)
+                vm.debug14++;
+            else
+                vm.debug14--;
+        }
 
         function activate() {
             $scope.$on('closeRequest', function () {
