03aa1d61ce1d1b9e7ff99c287f7ef6bf2de6483e
Edvard <edvardlindelof@gmail.com>
2016-07-07 14:31:22 +0200

    lazyLoaded dummy-content to be found in Eds Widget

---
+++ gui/web/src/app/EdsWidget/EdsWidget.module.js
@@ -0,0 +1,8 @@
+(function() {
+    'use strict';
+
+    angular.module('app.EdsWidget', [
+        'app.core',
+        'app.widgets'
+      ]);
+})();
--- gui/web/src/app/core/config.js
+++ gui/web/src/app/core/config.js
@@ -33,5 +33,18 @@
         dialogsProvider.setSize('md');
         dialogsProvider.useFontAwesome();
     }
+    
+    core.config(function($ocLazyLoadProvider) {
+        $ocLazyLoadProvider.config({
+            //debug: true,
+            //events: true,
+            modules: [{
+                name: 'gridModule',
+                files: [
+                    'js/gridModule.js'
+                ]
+            }]
+        });
+    });
 
 })();
