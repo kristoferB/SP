6170b0ea4b9769581e0b28c3747b81f1c18166bd
Edvard <edvardlindelof@gmail.com>
2016-07-11 15:35:21 +0200

    försök till ng2 i 1..

---
+++ gui/web/ng2things/main.ts
@@ -0,0 +1,13 @@
+/*
+import { HeroDetailComponent } from './hero-detail.component';
+import {UpgradeAdapter} from '@angular/upgrade';
+
+declare var angular: any;
+
+var adapter: UpgradeAdapter = new UpgradeAdapter();
+
+angular.module('myApp')
+  .directive('heroDetail', adapter.downgradeNg2Component(HeroDetailComponent));
+adapter.bootstrap(document.body, ['myApp']);
+*/
+console.log("we here?");
