b3bcc089b188bc0d00c17cdcaf5722e5954cf7ca
marptt <laxenmeflaxen+git@gmail.com>
2016-07-12 15:19:27 +0200

    added an ugly lock-panel button, tweaked the .less file
    moved the gridsterOptions object

--- gui/web/src/app/settings/settings-service.js
+++ gui/web/src/app/settings/settings-service.js
@@ -5,27 +5,33 @@
         .module('app.settings')
         .factory('settingsService', settingsService);
 
-    settingsService.$inject = ['$localStorage', '$http'];
+    settingsService.$inject = ['$localStorage', '$http', 'dashboardService'];
     /* @ngInject */
-    function settingsService($localStorage, $http) {
+    function settingsService($localStorage, $http, dashboardService) {
         //For debugging: clears localstorage
-        //$localStorage.$reset();
+        $localStorage.$reset();
         
+        // contains defaults for  all options that cannot be defined 
+        // directly in the .less file.
+        var defaults = {
+            color: {
+                default_white: {}
+            },
+            layout:{
+                standard: {
+                    gridster_panel_margin: 10
+                }
+            }                 
+        };
         var service = {
             storage: $localStorage.$default({
-                saved_color_themes: {default_white: {}},
-                saved_layout_themes: {
-                    standard: {
-                        gridster_panel_margin: 10
-                    }
-                },
+                saved_color_themes: defaults.color,
+                saved_layout_themes: defaults.layout,
 
                 current_color_theme: 'default_white',
-                current_layout_theme: 'standard',
-
-                gridlock: false
-                
+                current_layout_theme: 'standard'
             }),
+
             availible_options: {
                 saved_color_themes: ['default_white'],
                 saved_layout_themes: ['standard']
@@ -33,10 +39,14 @@
             
             showHeaders: true,
             showNavbar: true,
-            theme_refreshed: theme_refreshed,
+            lockEnabled: true,
+
+            compile_less: compile_less,
             getLayoutTheme: getLayoutTheme,
             getColorTheme: getColorTheme,
-            toggleNavbar: toggleNavbar
+            toggleNavbar: toggleNavbar,
+            togglePanelLock: togglePanelLock,
+            update: update
         };
 
         activate();
@@ -45,9 +55,15 @@
 
         function activate() {
             fetchSavedConfigs();
-            theme_refreshed();
         }
-        function theme_refreshed(){
+
+        function configureGridster(){
+            var margin = ($localStorage.saved_layout_themes[ $localStorage.current_layout_theme]).gridster_panel_margin;
+            console.log(margin);
+            dashboardService.setPanelMargins(margin);
+        }
+
+        function compile_less(){
             //merge config variables into the .less file
             less.modifyVars(
                 Object.assign(
@@ -61,39 +77,48 @@
 
         function toggleNavbar(){
             service.showNavbar = !service.showNavbar;
-            theme_refreshed();
-        }
-
-        function toggleHeaders(){
-            service.showHeaders = !service.showHeaders;
-            theme_refreshed();
+            update();
         }
 
         function getColorTheme(){
-            return $localStorage.saved_color_themes[ $localStorage.current_color_theme];
+            return $localStorage.saved_color_themes[ 
+                $localStorage.current_color_theme];
         }
 
         function getLayoutTheme(){
-            return $localStorage.saved_layout_themes[$localStorage.current_layout_theme];
+            return $localStorage.saved_layout_themes[
+                $localStorage.current_layout_theme];
         }
 
         function fetchSavedConfigs(){
-            assignFromdatabase("/style_presets/layout_presets.json", "saved_layout_themes");
-            assignFromdatabase("/style_presets/color_presets.json", "saved_color_themes");
+            assignFromServer("/style_presets/layout_presets.json", "saved_layout_themes");
+            assignFromServer("/style_presets/color_presets.json", "saved_color_themes");
+            update();
         }
 
-        function assignFromdatabase(url, variable){
+        function togglePanelLock(){
+            service.lockEnabled = !service.lockEnabled;
+            dashboardService.setPanelLock(service.lockEnabled);
+        }
+
+        function update(){
+            compile_less();
+            console.log('oh shit waddup');
+            configureGridster();
+        }
+
+        function assignFromServer(url, variable){
+            //fetches data from url and saves it in $localStorage.variable
             $http.get(url, "json").
                 then(function successCallback(response) {
-                    console.log(response);
                     $localStorage[variable] = response.data;
                     service.availible_options[variable] = [];
                     for(var key in response.data){
                         service.availible_options[variable].push(key);    
                     }
-                    theme_refreshed();
+                    update();
                 }, function errorCallback(response) {
-                    console.log("TODO: handle this error better");
+                    console.log(response);
                 }
             );
         }
