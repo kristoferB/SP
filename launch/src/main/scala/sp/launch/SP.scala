0fc8c4964279ecebde4e6425efab5eb965285133
Martin Dahl <martin.dahl@gmail.com>
2016-08-23 14:44:23 +0200

    oscar testing

--- launch/src/main/scala/sp/launch/SP.scala
+++ launch/src/main/scala/sp/launch/SP.scala
@@ -25,15 +25,21 @@ class VolvoRobots(params: SPAttributes) extends CPModel {
     val durations = Array(1600,1000,1600,400,600,200,800,200,200,1200,1000,600,4618,2534,1539,2895,800,800,1000,200,2000);
     val totalDuration = durations.sum
 
-
     val precedences = List(("RS1_B940WeldSeg1","RS1_B940WeldSeg2"),("RS1_B940WeldSeg2","RS1_B941WeldSeg3"),("RS1_B941WeldSeg3","RS1_B940WeldSeg4"),("RS2_B940WeldSeg1","RS2_B941WeldSeg2"),("RS2_B941WeldSeg2","RS2_B940WeldSeg3"),("RS2_B940WeldSeg3","RS2_B940WeldSeg4"),("RS2_B940WeldSeg4","RS2_B941WeldSeg5"),("RS2_B941WeldSeg5","RS2_B940WeldSeg6"),("RS2_B940WeldSeg6","RS2_B941WeldSeg8"),("RS2_B941WeldSeg8","RS2_B941WeldSeg9"),("RS3_B941WeldSeg1","RS3_B940WeldSeg2"),("RS3_B940WeldSeg2","RS3_B941WeldSeg3"),("RS3_B941WeldSeg3","RS3_B940WeldSeg4"),("RS4_B940WeldSeg1","RS4_B940WeldSeg2"),("RS4_B940WeldSeg2","RS4_B941WeldSeg3"),("RS4_B941WeldSeg3","RS4_B940WeldSeg4"),("RS4_B940WeldSeg4","RS4_B940WeldSeg5"))
 
     val mutexes = List(("RS4_B941WeldSeg3", "RS2_B941WeldSeg2"), ("RS4_B940WeldSeg4", "RS2_B940WeldSeg1"), ("RS4_B941WeldSeg3", "RS2_B940WeldSeg3"), ("RS4_B940WeldSeg2", "RS2_B941WeldSeg2"), ("RS4_B940WeldSeg2", "RS2_B940WeldSeg3"), ("RS4_B940WeldSeg2", "RS2_B940WeldSeg1"), ("RS4_B941WeldSeg3", "RS2_B940WeldSeg1"), ("RS4_B940WeldSeg4", "RS2_B941WeldSeg2"), ("RS4_B940WeldSeg4", "RS2_B940WeldSeg3"),("RS4_B940WeldSeg2", "RS1_B941WeldSeg3"), ("RS4_B941WeldSeg3", "RS1_B940WeldSeg4"), ("RS4_B940WeldSeg1", "RS1_B940WeldSeg4"), ("RS4_B941WeldSeg3", "RS1_B941WeldSeg3"), ("RS4_B940WeldSeg1", "RS1_B941WeldSeg3"), ("RS4_B940WeldSeg2", "RS1_B940WeldSeg4"), ("RS1_B941WeldSeg3", "RS4_B940WeldSeg4"), ("RS4_B940WeldSeg4", "RS1_B940WeldSeg4"),("RS3_B940WeldSeg4", "RS1_B940WeldSeg1"), ("RS3_B940WeldSeg2", "RS1_B940WeldSeg1"), ("RS3_B941WeldSeg3", "RS1_B940WeldSeg1"),("RS4_B940WeldSeg5", "RS2_B940WeldSeg4"), ("RS4_B940WeldSeg5", "RS2_B940WeldSeg1"), ("RS4_B940WeldSeg5", "RS2_B941WeldSeg2"), ("RS4_B940WeldSeg5", "RS2_B940WeldSeg3"),("RS2_B941WeldSeg8", "RS3_B941WeldSeg1"))
+
+    val forceEndTimes = List(("RS1_B940WeldSeg2","RS1_B941WeldSeg3"),
+      ("RS1_B941WeldSeg3","RS1_B940WeldSeg4"),("RS2_B940WeldSeg1","RS2_B941WeldSeg2"),
+      ("RS2_B941WeldSeg2","RS2_B940WeldSeg3"),("RS2_B941WeldSeg5","RS2_B940WeldSeg6"),
+      ("RS3_B941WeldSeg3","RS3_B940WeldSeg4"),("RS4_B940WeldSeg2","RS4_B941WeldSeg3"),
+      ("RS4_B941WeldSeg3","RS4_B940WeldSeg4"))
  
     var startTimes = Array.fill(numTasks)(CPIntVar(0, totalDuration))
     var endTimes = Array.fill(numTasks)(CPIntVar(0, totalDuration))
     var makespan = CPIntVar(0 to totalDuration)
 
+    forceEndTimes.foreach { case (t1,t2) => add(startTimes(nameMap(t1)) + durations(nameMap(t1)) == startTimes(nameMap(t2)) ) }
     precedences.foreach { case (t1,t2) => add(startTimes(nameMap(t1)) + durations(nameMap(t1)) <= startTimes(nameMap(t2)) ) }
     mutexes.foreach { case (t1,t2) =>
       val leq1 = startTimes(nameMap(t1)) + durations(nameMap(t1)) <== startTimes(nameMap(t2))
@@ -52,15 +58,19 @@ class VolvoRobots(params: SPAttributes) extends CPModel {
     }
     add(maximum(endTimes, makespan))
 
-    minimize(makespan)
-    search(binaryStatic(startTimes ++ Array(makespan)))
+//    minimize(makespan)
+    search(binaryFirstFail(startTimes ++ Array(makespan)))
 
+    var sols = Map[Int, Int]()
     onSolution {
-      println("Makespan: " + makespan)
+      sols += makespan.value -> (sols.get(makespan.value).getOrElse(0) + 1)
+      println("Makespan: " + makespan.value)
       println("Start times: ")
       names.foreach { name =>
-        println(name + ": " + startTimes(nameMap(name)))
+        println(name + ": " + startTimes(nameMap(name)).value + " - " +
+          durations(nameMap(name)) + " --> " + endTimes(nameMap(name)).value)
       }
+      sols.foreach { case (k,v) => println(k + ": " + v + " solutions") }
     }
 
     val stats = start() // (nSols = 1)
