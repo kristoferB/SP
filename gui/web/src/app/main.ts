8ffd710c855a8ffee131fdd4823feafd39ea707f
Martin <martiiin.pettersson@gmail.com>
2016-09-08 15:45:40 +0200

    fixed package.json being brokend and minor code formatting fixes

--- gui/web/src/app/main.ts
+++ gui/web/src/app/main.ts
@@ -1,7 +1,7 @@
 import { upgAdapter } from './upg-helpers/upg-adapter';
-import {upgConvertStuff} from './upg-helpers/upg-convert-stuff';
-import {Ng2DashboardService} from "./dashboard/ng2-dashboard.service";
-import {Ng2DashboardComponent} from "./dashboard/ng2-dashboard.component";
+import { upgConvertStuff } from './upg-helpers/upg-convert-stuff';
+import { Ng2DashboardService } from "./dashboard/ng2-dashboard.service";
+import { Ng2DashboardComponent } from "./dashboard/ng2-dashboard.component";
 
 upgConvertStuff(upgAdapter);
 
--- gui/web/src/app/widget-kinds.ts
+++ gui/web/src/app/widget-kinds.ts
@@ -18,18 +18,18 @@ import {
 } from './upg-helpers/upg-ng1-widget-containers';
 
 export const widgetKinds: WidgetKind[] = [
-  	{ 'component': Faces, 'title': 'ERICA Faces', 				'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
-  	{ 'component': AwesomeNG2Component, 'title': 'ng2Inside', 		'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
-   	{ 'component': KubInputGUIComponent, 'title': 'upg-kub', 		'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
-   	{ 'component': ItemExplorerComponent, 'title': 'item-explorer', 	'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
-    	{ 'component': ItemEditorComponent, 'title': 'item-editor', 		'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
-    	{ 'component': SOPMakerComponent, 'title': 'sop-maker', 		'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
-    	{ 'component': OPCRunnerComponent, 'title': 'OPC-runner', 		'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
-    	{ 'component': ProcessSimulateComponent, 'title': 'process-simulate', 	'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
-    	{ 'component': ConditionEditorComponent, 'title': 'condition-editor', 	'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
-    	{ 'component': OperationControlComponent, 'title': 'operation-control', 'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
-    	{ 'component': OperatorInstGUIComponent, 'title': 'operator-inst-gui', 	'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
-    	{ 'component': RobotCycleAnalysisComponent, 'title': 'robot-cycle', 	'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
-    	{ 'component': ActiveOrderComponent, 'title': 'active-order', 		'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
-    	{ 'component': Tobbe2Component, 'title': 'tobbe-two', 			'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null }
+    { 'component': Faces, 'title': 'ERICA Faces', 'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
+    { 'component': AwesomeNG2Component, 'title': 'ng2Inside', 'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
+    { 'component': KubInputGUIComponent, 'title': 'upg-kub', 'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
+    { 'component': ItemExplorerComponent, 'title': 'item-explorer', 'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
+    { 'component': ItemEditorComponent, 'title': 'item-editor', 'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
+    { 'component': SOPMakerComponent, 'title': 'sop-maker', 'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
+    { 'component': OPCRunnerComponent, 'title': 'OPC-runner', 'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
+    { 'component': ProcessSimulateComponent, 'title': 'process-simulate', 'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
+    { 'component': ConditionEditorComponent, 'title': 'condition-editor', 'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
+    { 'component': OperationControlComponent, 'title': 'operation-control', 'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
+    { 'component': OperatorInstGUIComponent, 'title': 'operator-inst-gui', 'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
+    { 'component': RobotCycleAnalysisComponent, 'title': 'robot-cycle', 'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
+    { 'component': ActiveOrderComponent, 'title': 'active-order', 'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null },
+    { 'component': Tobbe2Component, 'title': 'tobbe-two', 'sizex': 4, 'sizey': 4, 'id': null, 'gridOptions': null }
 ]
