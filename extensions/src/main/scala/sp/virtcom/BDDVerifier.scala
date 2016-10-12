37aa1b54b7cd44d873e78d72c3c8371dfad672d5
Martin Dahl <martin.dahl@gmail.com>
2016-10-12 21:04:56 +0200

    Use BDD to check what can happen

--- extensions/src/main/scala/sp/virtcom/BDDVerifier.scala
+++ extensions/src/main/scala/sp/virtcom/BDDVerifier.scala
@@ -14,9 +14,6 @@ import org.json4s.JString
 
 import scala.util._
 
-
-case class BDDArg(bdd: Map[String, Int] => Option[Boolean])
-
 object BDDVerifier extends SPService {
   val specification = SPAttributes()
   val transformTuple = (
@@ -24,12 +21,10 @@ object BDDVerifier extends SPService {
   )
   val transformation = transformToList(transformTuple.productIterator.toList)
 
-  def props(modelHandler: ActorRef, eventHandler: ActorRef) = Props(classOf[BDDVerifier], modelHandler, eventHandler)
+  def props(modelHandler: ActorRef) = Props(classOf[BDDVerifier], modelHandler)
 }
 
-
-
-class BDDVerifier(modelHandler: ActorRef, eventHandler: ActorRef) extends Actor with ServiceSupport {
+class BDDVerifier(modelHandler: ActorRef) extends Actor with ServiceSupport {
   implicit val timeout = Timeout(100.seconds)
   val serviceID = ID.newID
   var bdds: Map[String, Map[String, Int] => Option[Boolean]] = Map()
@@ -39,19 +34,39 @@ class BDDVerifier(modelHandler: ActorRef, eventHandler: ActorRef) extends Actor 
   def receive = {
     case r@Request(service, attr, ids, reqID) => {
       val replyTo = sender()
+      println("bdd verifier got request:");
+      println(attr)
       implicit val rnr = RequestNReply(r, replyTo)
       val command = transform(BDDVerifier.transformTuple)
       for {
         c <- r.attributes.getAs[ServiceHandlerAttributes]("core")
         m <- c.model
       } yield {
-
+        println("bdd verifier got request: " + command);
+        val bddName = command.getAs[String]("bdd").getOrElse("")
+        val f = bdds.get(bddName).getOrElse((_ => None): Map[String,Int] => Option[Boolean])
+        val partialState = command.getAs[Map[String,Int]]("partialState").getOrElse(Map())
+        println("State: " + partialState)
+        val resp = f(partialState) match {
+          case Some(result) => Response(List(), SPAttributes("silent"->true, "result"->result), service, serviceID)
+          case None => Response(List(), SPAttributes("silent"->true, "invalid"->true), service, serviceID)
+        }
+        replyTo ! resp
       }
 
-      replyTo ! Response(List(), SPAttributes("silent"->true), service, serviceID)
     }
 
-    case x: BDDArg => bdds += "test" -> x.bdd
+    case RegisterBDD(name, bdd, _) =>
+      println("BDD " + name + " registered")
+      bdds += name -> bdd
+
+      // test code
+      // List(0,1,2,3).foreach { n =>
+      //   bdd(Map("vChamber_status"->n,"vRobot_pos"->2)) match {
+      //     case Some(result) => println("Chamber state: " + n + ", can weld? " + result)
+      //     case None => println("No BDD exists :(")
+      //   }
+      // }
 
     case _ => sender ! SPError("Ill formed request")
   }
