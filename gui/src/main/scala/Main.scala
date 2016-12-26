52e29946bcfb47a297e7b25ae6b4c26550231406
Edvard <edvardlindelof@gmail.com>
2016-12-26 21:05:51 +0100

    cleaned up Main; now only responsible for strapping the vdom

--- gui/src/main/scala/Main.scala
+++ gui/src/main/scala/Main.scala
@@ -4,31 +4,10 @@ import scala.scalajs.js.JSApp
 import japgolly.scalajs.react._
 import org.scalajs.dom.document
 import scala.scalajs.js.annotation.JSExport
-import monix.execution.Scheduler.Implicits.global
 
 object Main extends JSApp {
   @JSExport
   override def main(): Unit = {
-    // this should maybe actually be a singleton
-    val componentLoaderService = new ComponentLoaderService()
-
-    /*
-    // trying out the loader, for now using a hard link 
-    componentLoaderService.fetchFactory(
-      "http://localhost:3000/build/sp-example-widget.js", "TestWidget")
-      .onComplete( res => {
-        // create a widget from the factory
-        // i have not gone object-oriented-crazy: this is how react needs things to work. i think.
-        val widget = res.get.apply(WrapObj(Unit))
-
-        val widgetWrapper = new WidgetWrapper(widget)
-
-        // inject the component into DOM
-        //ReactDOM.render(widgetWrapper.getComponent(), document.body)
-        ReactDOM.render(Grid.component(), document.body)
-      }
-    )
-    */
     ReactDOM.render(Layout(), document.getElementById("spgui-root"))
   }
 }
