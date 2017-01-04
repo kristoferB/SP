a87d19f4ffc23d4c04db8db59535508e853e6998
Kristofer Bengtsson <kristofer@sekvensa.se>
2017-01-04 14:57:06 +0100

    Added comm with backend

--- gui/src/main/scala/widgets/WidgetCommTest.scala
+++ gui/src/main/scala/widgets/WidgetCommTest.scala
@@ -2,10 +2,31 @@ package spgui.widgets
 
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.prefix_<^._
+
 import scala.concurrent.Future
 import scala.concurrent.Promise
+import fr.hmil.roshttp.HttpRequest
+import monix.execution.Scheduler.Implicits.global
 
-import fr.hmil.roshttp._
+import scala.util.{Failure, Success}
+import fr.hmil.roshttp.response.SimpleHttpResponse
+
+import scala.reflect.ClassTag
+
+object APITEST {
+  sealed trait API
+  case class Test1(p1: String, p2: String) extends API
+  case class Test2(p1: Int, p2: Int) extends API
+  case class Test3(p1: Double, p2: Tom) extends API
+
+  sealed trait SUB
+  case class Tom(str: String) extends SUB
+
+  lazy val apiClasses: List[Class[_]] =   sp.macros.MacroMagic.values[API]
+  lazy val apiJson: List[String] = sp.macros.MacroMagic.info[API, SUB]
+
+}
+
 
 object WidgetCommTest {
 
@@ -25,7 +46,7 @@ object WidgetCommTest {
         <.br(),
         <.button(
           ^.className := "btn btn-default",
-          ^.onClick --> send, "SEND"
+          ^.onClick --> Callback.future(send), "SEND"
         )
       )
 
@@ -33,9 +54,30 @@ object WidgetCommTest {
       changeState(e.target.value)
     }
 
-    def send(): Callback = {
-      Callback.alert("CLICKED")
+    def send(): Future[Callback] = {
+      HoHo
     }
+
+    def HoHo = {
+      import fr.hmil.roshttp.body._
+      val request = HttpRequest("http://localhost:8080/test")
+      val fGet = request.get()
+      val mess = FixedType.write(APITEST.Test1("ja", "JA"))
+      val fPost = request.post(PlainTextBody(mess))
+
+      for {
+        res <- fGet
+        p <- fPost
+      } yield {
+        val test1 = FixedType.read[APITEST.API](res.body)
+        Callback.log("tjo") >>
+          Callback.alert(test1.toString) >>
+          Callback.alert(p.body)
+      }
+
+
+    }
+
   }
 
 
@@ -45,4 +87,21 @@ object WidgetCommTest {
     .build
 
   def apply(): ReactElement = component()
+}
+
+
+import upickle._
+object FixedType extends upickle.AttributeTagged {
+  override val tagName = "isa"
+
+  override def annotate[V: ClassTag](rw: Reader[V], n: String) = Reader[V]{
+    case Js.Obj(x@_*) if x.contains((tagName, Js.Str(n.split('.').takeRight(2).mkString(".")))) =>
+      rw.read(Js.Obj(x.filter(_._1 != tagName):_*))
+  }
+
+  override def annotate[V: ClassTag](rw: Writer[V], n: String) = Writer[V]{ case x: V =>
+    val filter = n.split('.').takeRight(2).mkString(".")
+    Js.Obj((tagName, Js.Str(filter)) +: rw.write(x).asInstanceOf[Js.Obj].value:_*)
+  }
+
 }
