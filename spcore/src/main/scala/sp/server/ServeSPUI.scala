397d6f2a49a2cc330e085374173db5f59ed0f8c4
Kristofer Bengtsson <kristofer@sekvensa.se>
2017-02-08 10:10:49 +0100

    Launch file for SP core

---
+++ spcore/src/main/scala/sp/server/ServeSPUI.scala
@@ -0,0 +1,35 @@
+package sp.server
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import scala.io.StdIn
+
+/**
+  * Created by kristofer on 2017-01-02.
+  */
+class ServeSPUI extends App{
+
+  implicit val system = ActorSystem("my-system")
+  implicit val materializer = ActorMaterializer()
+  implicit val executionContext = system.dispatcher
+
+  val route =
+    path("hello") {
+      get {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+      }
+    }
+
+
+  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  StdIn.readLine() // let it run until user presses return
+  bindingFuture
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done
+
+}
