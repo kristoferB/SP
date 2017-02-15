46513fa0dae967957f33fcfe959ed0003fd8f8c4
Kristofer Bengtsson <kristofer@sekvensa.se>
2017-02-15 16:20:34 +0100

    Waiting for cluster up before actors starts

--- spservices/labkit/src/main/scala/sp/labkit/Launch.scala
+++ spservices/labkit/src/main/scala/sp/labkit/Launch.scala
@@ -5,14 +5,18 @@ import akka.actor._
 
 object Launch extends App {
   implicit val system = ActorSystem("SP")
+  
+  val cluster = akka.cluster.Cluster(system)
+
+  cluster.registerOnMemberUp{
+    system.actorOf(OPC.props, "OPC")
+    system.actorOf(OPMakerLabKit.props, "opMakerLabKit")
+    system.actorOf(ProductAggregator.props, "ProductAggregator")
+    system.actorOf(ResourceAggregator.props, "ResourceAggregator")
+  }
 
   // Add root actors used in node here
-  system.actorOf(OPC.props, "OPC")
-  system.actorOf(OPMakerLabKit.props, "opMakerLabKit")
-  system.actorOf(ProductAggregator.props, "ProductAggregator")
-  system.actorOf(ResourceAggregator.props, "ResourceAggregator")
 
-  val cluster = akka.cluster.Cluster(system)
   scala.io.StdIn.readLine("Press ENTER to exit application.\n") match {
     case x =>
       cluster.leave(cluster.selfAddress)
