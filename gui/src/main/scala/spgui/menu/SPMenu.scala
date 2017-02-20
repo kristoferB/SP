3e87528831d80e3717556f230c292d39e005016b
Martin <martiiin.pettersson@gmail.com>
2017-02-20 13:46:40 +0100

    bootstrap dashboard

--- gui/src/main/scala/spgui/menu/SPMenu.scala
+++ gui/src/main/scala/spgui/menu/SPMenu.scala
@@ -2,30 +2,52 @@ package spgui.menu
 
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.prefix_<^._
+import org.scalajs.dom._
+
+
 
 object SPMenu {
   private val component = ReactComponentB[Unit]("SPMenu")
     .render(_ =>
     <.nav(
+      ^.className := "navbar navbar-default",
       ^.className := SPMenuCSS.topNav.htmlClass,
-      ^.className := "navbar navbar-static-top navbar-default",
-      <.ul(
-        ^.className := SPMenuCSS.buttonList.htmlClass,
-        ^.className := "nav_navbar-nav",
-        ^.className := SPMenuCSS.navbarCell.htmlClass,
-        WidgetMenu()
+      <.div(
+        ^.className := "navbar-header",
+        <.div(
+          ^.className := SPMenuCSS.spLogoDiv.htmlClass,
+          spLogo
+        )
       ),
-      spLogo
+      <.div(
+        <.div(
+        ^.className := "navbar-toggle collapsed",
+        "Navbar has collapsed, i am a placeholder as width < 768px"
+      ),
+        ^.className := "container-fluid",
+        ^.className := SPMenuCSS.container.htmlClass,
+        <.ul(
+          ^.id := "navbar-collapse-id",
+          ^.className := "collapse navbar-collapse",
+          ^.className := SPMenuCSS.buttonList.htmlClass,
+          ^.className := "nav navbar-nav",
+          ^.className := SPMenuCSS.navbarCell.htmlClass,
+          WidgetMenu()
+        )
+      )
     )
   )
     .build
 
+
+
   private val spLogo = (
-      <.ul(
+    <.div(
+      ^.className := SPMenuCSS.splogoContainer.htmlClass,
+      <.div(
         ^.className := SPMenuCSS.spLogo.htmlClass,
         ^.className := SPMenuCSS.navbarCell.htmlClass
       )
-    )
-
+    ))
   def apply() = component()
 }
