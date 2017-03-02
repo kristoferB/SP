4654b1f5bd3898bf735ae6dc45e0b6992062fbf6
Kristofer Bengtsson <kristofer@sekvensa.se>
2017-03-02 09:17:45 +0100

    VD WIP

--- spservices/devicehandler/src/main/scala/sp/devicehandler/VirtualDevice.scala
+++ spservices/devicehandler/src/main/scala/sp/devicehandler/VirtualDevice.scala
@@ -44,6 +44,9 @@ package APIVirtualDevice {
   // requests command (gets a SPACK and when applied, SPDone (and indirectly a StateEvent))
   case class ResourceCommand(resource: String, stateRequest: Map[String, SPValue], timeout: Int = 0) extends Requests
 
+  // requests from driver
+  case class DriverStateChange(name: String, id: UUID, state: Map[String, SPValue], diff: Boolean = false)  extends Requests
+
 
   // answers
   sealed trait Replies
@@ -53,6 +56,7 @@ package APIVirtualDevice {
   case class Drivers(xs: List[Driver]) extends Replies
   case class NewResource(x: Resource) extends Replies
   case class RemovedResource(x: Resource) extends Replies
+  case class NewDriver(x: Driver) extends Replies
   case class RemovedDriver(x: Driver) extends Replies
 
 
@@ -107,5 +111,63 @@ class VirtualDevice(name: String, id: UUID) extends PersistentActor with ActorLo
 
 
 trait VirtualDeviceLogic {
-  var devices: 
+  val name: String
+  val id: UUID
+
+  type StateMap = Map[String, SPValue]
+  case class StateMapper(f: (Map[String, StateMap], Map[String, StateMap]) =>  Map[String, StateMap])
+  val defMapper = StateMapper { (in, out) =>
+    val current = out(name)
+    val upd = in.foldLeft(current){(a, b) =>
+      val prefixState = b._2.map(kv => s"${b._1}.${kv._1}"->kv._2)
+
+    }
+
+  }
+  val defResource = api.Resource(name, id, List(), SPAttributes, false)
+
+
+  var drivers: Map[String, api.Driver] = Map()
+  var driverState: Map[String, StateMap] = Map()
+  var resources: Map[String, api.Resource] = Map(defResource.name -> defResource)
+  var resourceState: Map[String, StateMap] = Map()
+
+  var driverToResMapper: List[StateMapper] = List()
+  var resToDriverMapper: List[StateMapper] = List()
+
+
+
+  def newDriver(d: api.Driver) = {
+    drivers += d.name -> d
+  }
+
+  def newResource(resource: api.Resource) = {
+
+  }
+
+  def driverEvent(e: api.DriverStateChange) = {
+
+  }
+
+
+
+}
+
+trait VDDriverComm {
+  val context: ActorContext
+  import context.dispatcher
+  val mediator: ActorRef
+
+
+}
+
+trait VDMappers {
+  type StateMap = Map[String, SPValue]
+  def flatMapState(thingState: Map[String, State], prefix: Boolean) = {
+    thingState.foldLeft(Map[String, SPValue]()){(a, b) =>
+      val pref = if (prefix) b._1+"." else ""
+      val prefixState = b._2.map(kv => s"${pref}${kv._1}"->kv._2)
+      a ++ prefixState
+  }
+
 }
