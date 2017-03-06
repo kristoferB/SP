371b21008ac89cafec22677f8288b2c7137a0825
Edvard Lindelöf <edvard@Bulldozer2.BulldozerHome>
2017-03-06 17:25:50 +0100

    tree: state into own file and made general

---
+++ gui/src/main/scala/spgui/widgets/itemexplorer/RootDirectory.scala
@@ -0,0 +1,47 @@
+package spgui.widgets.itemexplorer
+
+trait DirectoryItem {
+  val name: String
+  // TODO: will be a string/UUID
+  val id: Int
+}
+case class Directory(name: String, id: Int, childrenIds: List[Int]) extends DirectoryItem
+
+class RootDirectory(private var _items: List[DirectoryItem], private var _rootLevelItemIds: List[Int]) {
+  // parentIdMap(i) points to to parent of item of id i, undefined if item is on root level
+  private var parentIdMap: Map[Int, Int] = _items.flatMap{
+      case Directory(_, id, childrenIds) => childrenIds.map((_, id))
+      case _ => Nil
+    }.toMap
+
+  // apparently the only scala way of stopping the client from modifiying a var
+  def items = _items
+  def rootLevelItemIds = _rootLevelItemIds
+
+  // return self, to make $.modstate calls look cleaner
+  def addItem(item: DirectoryItem) = {
+    _items = item :: _items
+    _rootLevelItemIds = item.id :: _rootLevelItemIds
+    this
+  }
+
+  // move item to parent of target if target is not a directory
+  def moveItem(movedItemId: Int, newParentId: Int) = _items.find(_.id == newParentId).get match {
+    case mapp: Directory => moveItemToDir(movedItemId, newParentId)
+    case item: DirectoryItem => moveItemToDir(movedItemId, parentIdMap(newParentId))
+  }
+
+  private def moveItemToDir(movedItemId: Int, newDirId: Int) = {
+    _items = _items.map{
+      case Directory(name, `newDirId`, childrenIds) => Directory(name, newDirId, movedItemId :: childrenIds)
+      case Directory(name, id, childrenIds) => Directory(name, id, childrenIds.filter(_ != movedItemId))
+      case item: DirectoryItem => item
+    }
+
+    if(!parentIdMap.contains(movedItemId)) _rootLevelItemIds = _rootLevelItemIds.filter(_ != movedItemId)
+
+    parentIdMap = parentIdMap.filterKeys(_ != movedItemId) ++ Map(movedItemId -> newDirId)
+
+    this
+  }
+}
--- gui/src/main/scala/spgui/widgets/itemexplorer/Tree.scala
+++ gui/src/main/scala/spgui/widgets/itemexplorer/Tree.scala
@@ -10,63 +10,17 @@ import spgui.components.DragAndDrop.{ DataOnDrag, OnDataDrop }
 import spgui.components.Dropdown
 
 // TODO: replace with SP API
-// this is just a dummy to have something to work with
-sealed abstract class Item {
-  val name: String
-  // will be an UUID
-  val id: Int
-}
-case class Mapp(name: String, id: Int, childrenIds: List[Int]) extends Item
-case class Spotify(name: String, id: Int, content: String) extends Item
-case class Youtube(name: String, id: Int, content: String) extends Item
+case class Spotify(name: String, id: Int, content: String) extends DirectoryItem
+case class Youtube(name: String, id: Int, content: String) extends DirectoryItem
 
 object TVButton {
   private def button(name: String, icon: ReactNode) =
     <.div(<.div(Style.icon, icon), name, <.div(Style.chevron, Icon.chevronRight))
 
-  def apply(item: Item) = item match {
-    case Mapp(name, _, _) => button(name, Icon.folder)
+  def apply(item: DirectoryItem) = item match {
+    case Directory(name, _, _) => button(name, Icon.folder)
     case Spotify(name, _, _) => button(name, Icon.spotify)
     case Youtube(name, _, _) => button(name, Icon.youtube)
-  }
-}
-
-class TreeState(private var _items: List[Item], private var _rootLevelItemIds: List[Int]) {
-  // parentIdMap(i) points to to parent of item of id i, undefined if item is on root level
-  private var parentIdMap: Map[Int, Int] = _items.flatMap{
-      case Mapp(_, id, childrenIds) => childrenIds.map((_, id))
-      case _ => Nil
-    }.toMap
-
-  // apparently the only scala way of stopping the client from modifiying a var
-  def items = _items
-  def rootLevelItemIds = _rootLevelItemIds
-
-  // return self, to make $.modstate calls look cleaner
-  def addItem(item: Item) = {
-    _items = item :: _items
-    _rootLevelItemIds = item.id :: _rootLevelItemIds
-    this
-  }
-
-  // move item to parent of target if target is not a directory
-  def moveItem(movedItemId: Int, newParentId: Int) = _items.find(_.id == newParentId).get match {
-    case mapp: Mapp => moveItemToDir(movedItemId, newParentId)
-    case item: Item => moveItemToDir(movedItemId, parentIdMap(newParentId))
-  }
-
-  private def moveItemToDir(movedItemId: Int, newDirId: Int) = {
-    _items = _items.map{
-      case Mapp(name, `newDirId`, childrenIds) => Mapp(name, newDirId, movedItemId :: childrenIds)
-      case Mapp(name, id, childrenIds) => Mapp(name, id, childrenIds.filter(_ != movedItemId))
-      case item: Item => item
-    }
-
-    if(!parentIdMap.contains(movedItemId)) _rootLevelItemIds = _rootLevelItemIds.filter(_ != movedItemId)
-
-    parentIdMap = parentIdMap.filterKeys(_ != movedItemId) ++ Map(movedItemId -> newDirId)
-
-    this
   }
 }
 
@@ -75,26 +29,26 @@ object ListItems {
     Youtube("Smör", 2, "mjölk"),
     Youtube("Ägg", 3, "kalcium"),
     Spotify("Kladd", 4, "Kladd"),
-    Mapp("kaka", 5, List(2, 3)),
+    Directory("kaka", 5, List(2, 3)),
     Spotify("Äpplen", 6, "paj")
   )
   val rootLevelItemIds = List(4, 5, 6)
-  def apply() = new TreeState(listItems, rootLevelItemIds)
+  def apply() = new RootDirectory(listItems, rootLevelItemIds)
 }
 
 object Tree {
-  def emptyMap() = Mapp("EmptyMap", util.Random.nextInt(1000000) + 10000, List())
+  def emptyMap() = Directory("EmptyMap", util.Random.nextInt(1000000) + 10000, List())
   def newSpotify() = Spotify("NewYT", util.Random.nextInt(1000000) + 10000, "content of NewSpotify")
   def newYT() = Youtube("NewYT", util.Random.nextInt(1000000) + 10000, "content of NewYT")
 
-  class TreeBackend($: BackendScope[Unit, TreeState]) {
-    def addItem(item: Item) = $.modState(_.addItem(item))
+  class TreeBackend($: BackendScope[Unit, RootDirectory]) {
+    def addItem(item: DirectoryItem) = $.modState(_.addItem(item))
 
     def onDrop(senderId: String, receiverId: String) =
       Callback.log(s"item of id $senderId dropped on item of id $receiverId") >>
         $.modState(_.moveItem(senderId.toInt, receiverId.toInt))
 
-    def render(s: TreeState) =
+    def render(s: RootDirectory) =
       <.div(
         Style.outerDiv,
         Dropdown(
@@ -123,7 +77,7 @@ object Tree {
 }
 
 object TVColumn {
-  case class Props(items: List[Item], itemIds: List[Int], onDrop: (String, String) => Callback)
+  case class Props(items: List[DirectoryItem], itemIds: List[Int], onDrop: (String, String) => Callback)
   case class State(selectedItemId: Int = -1)
 
   class TVColumnBackend($: BackendScope[Props, State]) {
@@ -154,7 +108,7 @@ object TVColumn {
         ),
         if(s.selectedItemId == -1) ""
         else p.items.find(_.id == s.selectedItemId).get match {
-          case Mapp(_, _, childrenIds) => TVColumn(p.items, childrenIds, p.onDrop)
+          case Directory(_, _, childrenIds) => TVColumn(p.items, childrenIds, p.onDrop)
           case Spotify(_, _, content) => content
           case Youtube(_, _, content) => content
         }
@@ -166,6 +120,6 @@ object TVColumn {
     .renderBackend[TVColumnBackend]
     .build
 
-  def apply(items: List[Item], itemIds: List[Int], onDrop: (String, String) => Callback): ReactElement =
+  def apply(items: List[DirectoryItem], itemIds: List[Int], onDrop: (String, String) => Callback): ReactElement =
     component(Props(items, itemIds, onDrop))
 }
