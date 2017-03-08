1c59fa1f0c22742e1158d27f712a90a99d3744cd
Kristofer Bengtsson <kristofer@sekvensa.se>
2017-03-08 13:39:54 +0100

    Some more tests

--- spservices/devicehandler/src/test/scala/sp/abilityhandler/AbilityActorTest.scala
+++ spservices/devicehandler/src/test/scala/sp/abilityhandler/AbilityActorTest.scala
@@ -38,9 +38,9 @@ class AbilityActorTest(_system: ActorSystem) extends TestKit(_system) with Impli
   val post = PropositionCondition(EQ(v1.id, 3), List(Action(v1.id, ValueHolder(4))))
   val started = PropositionCondition(EQ(v1.id, 2), List())
   val reset = PropositionCondition(AlwaysTrue, List(Action(v1.id, ValueHolder(1))))
-  val ability = api.Ability("test", ID.newID, pre, started, post, reset)
+  val ability = api.Ability("test", id, pre, started, post, reset)
 
-  var mh: ActorRef = system.actorOf(AbilityActor.props(ability), id.toString)
+
   override def beforeAll: Unit = {
 
   }
@@ -49,16 +49,39 @@ class AbilityActorTest(_system: ActorSystem) extends TestKit(_system) with Impli
     TestKit.shutdownActorSystem(system)
   }
 
-
+  import sp.abilityhandler.AbilityState._
 
   "Ability is created" - {
     "get ids" in {
+      var mh: ActorRef = system.actorOf(AbilityActor.props(ability))
       val probe = TestProbe()
       mh.tell(GetIds, probe.ref)
 
+      probe.fishForMessage(1 second){
+        case AbilityIds(aid, xs) => aid == id && xs.contains(v1.id)
+      }
+    }
+
+    "newState" in {
+      var mh: ActorRef = system.actorOf(AbilityActor.props(ability))
+      val probe = TestProbe()
+      mh.tell(NewState(Map(v1.id -> 1)), probe.ref)
+      mh.tell(NewState(Map(v1.id -> 2)), probe.ref)
 
       probe.fishForMessage(1 second){
-        case x => println("HEJ: " + x); false
+        case x @ AbilityStateChange(aid, s, cnt, reqID) if aid == id && s == enabled && cnt == 0 => false
+        case x @ AbilityStateChange(aid, s, cnt, reqID) if aid == id && s == executing && cnt == 1 => true
+      }
+    }
+    "start" in {
+      var mh: ActorRef = system.actorOf(AbilityActor.props(ability))
+      val probe = TestProbe()
+      val req = ID.newID
+      mh.tell(StartAbility(Map(v1.id -> 1), req, Map()), probe.ref)
+
+      probe.fishForMessage(1 second){
+        case x @ AbilityStateChange(aid, s, cnt, reqID) if aid == id && s == starting && cnt == 0 => false
+        case x @ StateUpdReq(aid, s) if aid == id && s == Map(v1.id -> SPValue(2)) => true
       }
     }
   }
