d02f759cc3166fbc53647ec8d5def116d50feb8f
Erik Risfelt <fredokin@gmail.com>
2017-03-10 09:56:20 +0100

    Added initial template for LKError.scala

---
+++ spservices/labkit/src/main/scala/sp/labkit/LKError.scala
@@ -0,0 +1,11 @@
+package sp.labkit
+
+/**
+  * Created by Erik on 10/03/2017.
+  */
+class LKError (val code: Int,
+               val description: String,
+               val solution: String
+              ) {
+//TODO Time stamp of some sort? Or is this part of exception?
+}
