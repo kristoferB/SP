2c7c02fae4037c6b83725d9566666811a4c17e24
Edvard Lindelöf <edvard@Bulldozer2.BulldozerHome>
2017-03-21 17:36:06 +0100

    editor: now looks for SPValue in SPWB (not seen if it works yet)

--- gui/src/main/scala/spgui/widgets/itemeditor/ItemEditor.scala
+++ gui/src/main/scala/spgui/widgets/itemeditor/ItemEditor.scala
@@ -8,6 +8,7 @@ import js.Dynamic.{ literal => l }
 import js.JSON
 
 import spgui.SPWidgetBase
+import sp.domain._
 
 object ItemEditor {
 
@@ -34,7 +35,7 @@ object ItemEditor {
           // dk if this is visible somewhere, but I think it would be nice
           "description" -> "UUID as string",
           "type" -> "string",
-          // jsoneditor doesn't, care, it seems, should facade onEditable
+          // jsoneditor doesn't care, it seems, should facade onEditable
           "readOnly" -> true
         )
       ),
@@ -42,36 +43,46 @@ object ItemEditor {
     )
   )
 
-  val json = l(
+  val sampleJson = l(
     "isa" -> "SomethingIncorrect",
     "name" -> "SampleJSON",
     "id" -> "this-should-be-an-uuid"
   )
 
+  class Backend($: BackendScope[SPWidgetBase, Unit]) {
+    def getDataOrSample(spV: SPValue) = if(spV == SPValue.empty) sampleJson else JSON.parse(spV.toJson)
 
-  // how to use a case class, convert it to SPValue, get the actual json, and how to get it back
-  import sp.domain._
-  import sp.messages.Pickles.fromJsonToSPValue
-
-  case class SomeCC(isa: String, id: String)
-  val someCC = SomeCC(isa = "något", id = "någotAnnat")
-
-  val spValue = SPValue(someCC)
-  println(spValue) // pretty prints spv as json
-
-  val spValueAsJSDynamic = JSON.parse(spValue.toJson)
-  println(spValueAsJSDynamic) // prints [object Object]
-
-  val retrievedSPVal = fromJsonToSPValue(JSON.stringify(spValueAsJSDynamic)).get // fromJson.. returns a Try
-  println(retrievedSPVal) // pretty prints spv as json
-
-  val retrievedCC = retrievedSPVal.getAs[SomeCC]("").get // getAs returns an Option
-  println(retrievedCC.id)
+    def render(spwb: SPWidgetBase) = <.div(
+      JSONEditor(options = jsonEditorOptions, json = getDataOrSample(spwb.getWidgetData)),
+      ItemEditorCSS.editor
+    )
+  }
 
   private val component = ReactComponentB[SPWidgetBase]("ItemEditor")
-    //.render_P(p => <.div(ItemEditorCSS.editor, JSONEditor(p.getWidgetData)))
-    .render_P(p => <.div(ItemEditorCSS.editor, JSONEditor(jsonEditorOptions, spValueAsJSDynamic)))
+    .renderBackend[Backend]
     .build
 
   def apply() = (spwb: SPWidgetBase) => component(spwb)
 }
+
+/*
+// how to use a case class, convert it to SPValue, turn it to actual json, and how to get it back to case class
+import sp.domain._
+import sp.messages.Pickles.fromJsonToSPValue
+
+case class SomeCC(isa: String, id: String)
+val someCC = SomeCC(isa = "något", id = "någotAnnat")
+
+val spValue = SPValue(someCC)
+println(spValue) // pretty prints spv as json
+
+val spValueAsJSDynamic = JSON.parse(spValue.toJson)
+println(spValueAsJSDynamic) // prints [object Object]
+
+val retrievedSPVal = fromJsonToSPValue(JSON.stringify(spValueAsJSDynamic)).get // fromJson.. returns a Try
+  println(retrievedSPVal) // pretty prints spv as json
+
+val retrievedCC = retrievedSPVal.getAs[SomeCC]("").get // getAs returns an Option
+println(retrievedCC.id) // prints "någotAnnat"
+ */
+
