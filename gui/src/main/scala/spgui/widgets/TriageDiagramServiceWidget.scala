f9f0c6cdfb3fcb1a2c47f210dcd9c619c1e3cc75
Ellen Korsberg <korsberg.ellen@gmail.com>
2017-04-05 16:50:59 +0200

    Added graphic for triage diagram

--- gui/src/main/scala/spgui/widgets/TriageDiagramServiceWidget.scala
+++ gui/src/main/scala/spgui/widgets/TriageDiagramServiceWidget.scala
@@ -34,6 +34,8 @@ import org.singlespaced.d3js.Ops._
 import scalacss.ScalaCssReact._
 import scalacss.Defaults._
 
+import scala.collection.mutable.ListBuffer
+
 import spgui.widgets.{API_PatientEvent => api}
 
 object TriageDiagramServiceWidget {
@@ -52,7 +54,7 @@ object TriageDiagramServiceWidget {
     )
   )
 
-  private class Backend($: BackendScope[Map[String, Triage], Unit]) {
+  private class Backend($: BackendScope[List[Double], Unit]) {
     spgui.widgets.css.WidgetStyles.addToDocument()
 
     val messObs = BackendCommunication.getMessageObserver(
@@ -114,84 +116,165 @@ object TriageDiagramServiceWidget {
     )
 
     // What is this function used for?
-    def render(p: Map[String, Triage]) = {
+    def render(p: List[Double]) = {
       <.div(^.`class` := "card-holder-root")( // really not necessary
       )
     }
   }
 
-  private val diagramHolderComponent = ReactComponentB[Map[String, Triage]]("diagramHolderComponent")
+  private val component = ReactComponentB[List[Double]]("teamVBelastning")
   .renderBackend[Backend]
-  .componentDidUpdate(dcb => Callback(addTriageDiagram(ReactDOM.findDOMNode(dcb.component), dcb.currentProps)))
+  .componentDidUpdate(dcb => Callback(addTheD3(ReactDOM.findDOMNode(dcb.component), dcb.currentProps)))
   .build
 
-  private def addTriageDiagram(element: raw.Element, apc: Map[String, Triage]): Unit = {
-    println("Lägger till triagediagram")
+  def dist(d: Double): Double = { // Bestämmer avstånd för antal patienter i widget.
+    if( d > 99){1}
+    else if( d > 9 ){10}
+    else {17.5}
+  }
 
-    d3.select(element).selectAll("*").remove() // clear all before rendering new data
+  def removeZero(d: Double): String = {
+    if(d.equals(0)){""}
+    else{d.toString()}
+  }
 
-    apc.values map ( p => {
-      val svg = d3.select(element).append("svg").attr("width", "300").attr("height", "200")
+  private def addTheD3(element: raw.Element, list: List[Double]): Unit = {
+     d3.select(element).selectAll("*").remove()
 
-      svg
-      .append("rect")
-      .attr("width", 300)
-      .attr("height", 200)
-      .attr("x", 0)
-      .attr("y", 0)
-      .attr("fill", "lightgrey")
+     val distance = 50
+     val width = 500
+     val height = 500
+     val barHeight = 350
+     val barWidth = 35
+     val currentTeam = "STREAMTEAM"
 
-      svg
-      .append("rect")
-      .attr("width", 80)
-      .attr("height", 200)
-      .attr("x", 0)
-      .attr("y", 0)
-      .attr("fill", "darkseagreen")
+     var length = new ListBuffer[Double]()
 
-      svg
-      .append("path")
-      .attr("class", "klinikfield")
-      .attr("d", "m 0,160 40,40 -40,0 z")
-      .attr("fill", "lightblue")
+     for(i <- 0 to 4){
+       length += (list(i)/(list(0) + list(1) + list(2) + list(3) + list(4)))*barHeight
+     }
 
-      svg
-      .append("text")
-      .attr("class", "location")
-      .attr("font-size", 25)
-      .attr("x", 20)
-      .attr("y", 50)
-      .attr("fill", "white")
-      .text("#gröna: " + p.colorMap("Grön"))
+     var scaleBars = barHeight / length.max
 
-      svg
-      .append("text")
-      .attr("font-size", 25)
-      .attr("x", 20)
-      .attr("y", 90)
-      .attr("fill", "white")
-      .text("#gula: " + p.colorMap("Gul"))
+     val svg = d3.select(element).append("svg")
+       .attr("width", width)
+       .attr("height", height)
 
-      svg
-      .append("text")
-      .attr("font-size", 25)
-      .attr("x", 20)
-      .attr("y", 130)
-      .attr("fill", "white")
-      .text("#orangea: " + p.colorMap("Orange"))
+     val g = svg.append("g")
+     // ------------------------ Graf ett BLÅ
+     g.append("rect")
+       .attr("x", distance)
+       .attr("y", 500 - length(0) * scaleBars)
+       .attr("width", barWidth)
+       .attr("height", length(0) * scaleBars)
+       .attr("fill", "#0000cd")
 
-      svg
-      .append("text")
-      .attr("font-size", 25)
-      .attr("x", 20)
-      .attr("y", 170)
-      .attr("fill", "white")
-      .text("#röda: " + p.colorMap("Röd"))
+     svg.append("text")
+       .attr("x", (dist(list(0)) + distance))
+       .attr("y", 495)
+       .attr("font-size", "20px")
+       .text(s"${removeZero(list(0))}")
+       .attr("fill", "#FFFFFF")
 
-    })
+     // ------------------------ Graf två GRÖN
+     g.append("rect")
+       .attr("x", 2*distance + barWidth)
+       .attr("y", 500 - length(1) * scaleBars)
+       .attr("width", barWidth)
+       .attr("height", length(1) * scaleBars)
+       .attr("fill", "#008000")
+
+     svg.append("text")
+       .attr("x", (dist(list(1)) + 2 * distance + barWidth))
+       .attr("y", 495)
+       .attr("font-size", "20px")
+       .text(s"${removeZero(list(1))}")
+       .attr("fill", "#FFFFFF")
+
+     // ------------------------ Graf tre GUL
+     g.append("rect")
+       .attr("x", 3*distance + 2*barWidth)
+       .attr("y", 500 - length(2) * scaleBars)
+       .attr("width", barWidth)
+       .attr("height", length(2) * scaleBars)
+       .attr("fill", "#ebeb00")
+
+     svg.append("text")
+       .attr("x", (dist(list(2)) + 3 * distance + 2 * barWidth))
+       .attr("y", 495)
+       .attr("font-size", "20px")
+       .text(s"${removeZero(list(2))}")
+       .attr("fill", "#FFFFFF")
+
+     // ------------------------ Graf fyra ORANGE
+     g.append("rect")
+       .attr("x", 4 * distance + 3 * barWidth)
+       .attr("y", 500 - length(3) * scaleBars)
+       .attr("width", barWidth)
+       .attr("height", length(3) * scaleBars)
+       .attr("fill", "#FFA500")
+
+     svg.append("text")
+       .attr("x", (dist(list(3)) + 4 * distance + 3 * barWidth))
+       .attr("y", 495)
+       .attr("font-size", "20px")
+       .text(s"${removeZero(list(3))}")
+       .attr("fill", "#FFFFFF")
+
+     // ------------------------ Graf fem RÖD
+     g.append("rect")
+       .attr("x",  5 * distance + 4 * barWidth)
+       .attr("y", 500 - length(4) * scaleBars)
+       .attr("width", barWidth)
+       .attr("height", length(4) * scaleBars)
+       .attr("fill", "#FF0000")
+
+     svg.append("text")
+       .attr("x", (dist(list(4)) +  5 * distance + 4 * barWidth))
+       .attr("y", 495)
+       .attr("font-size", "20px")
+       .text(s"${removeZero(list(4))}")
+       .attr("fill", "#FFFFFF")
+
+
+     // ------- TEAM ------------
+
+     svg.append("text")
+       .attr("x", 0)
+       .attr("y", 20)
+       .attr("font-size", "25px")
+       .text(currentTeam)
+       .style("font-weight", "bold")  //Helvetica som font
+       .attr("fill", "#000000")
+
+     // ------- ANTAL PATIENTER ------------
+
+     svg.append("text")
+       .attr("x", 0)
+       .attr("y", 50)
+       .attr("font-size", "20px")
+       .text(s"${list(0) + list(1) + list(2) + list(3) + list(4)}" + " " + "PATIENTER TOTALT")
+       .attr("fill", "#000000")
+
+     // ------- STATUS ------------
+
+     svg.append("text")
+       .attr("x", 0)
+       .attr("y", 100)
+       .attr("font-size", "15px")
+       .text("TRIAGE OCH BELASTNING")
+       .attr("fill", "#a8a8a8")
+
   }
 
   def apply() = spgui.SPWidget(spwb => {
-    diagramHolderComponent(triageCounter)
+    var listToSend = new ListBuffer[Double]()
+    listToSend += 0 // noll "svarta"
+    triageCounter.foreach{ t =>
+      t._2.colorMap.foreach{ c =>
+        listToSend += c._2.toDouble
+      }
+    }
+    component(listToSend.toList)
   })
 }
