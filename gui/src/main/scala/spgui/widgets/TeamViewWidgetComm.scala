82d5b22b4af934ff4b4dec6864989b240ad15f18
Ellen Korsberg <korsberg.ellen@gmail.com>
2017-04-21 14:40:53 +0200

    Added widget for each team/square.

--- gui/src/main/scala/spgui/widgets/TeamViewWidgetComm.scala
+++ gui/src/main/scala/spgui/widgets/TeamViewWidgetComm.scala
@@ -8,6 +8,7 @@ package API_PatientEvent {
 
   sealed trait PriorityEvent extends PatientProperty
   case class NotTriaged(careContactId: String, timestamp: String) extends PatientProperty with PriorityEvent
+  case class Blue(careContactId: String, timestamp: String) extends PatientProperty with PriorityEvent
   case class Green(careContactId: String, timestamp: String) extends PatientProperty with PriorityEvent
   case class Yellow(careContactId: String, timestamp: String) extends PatientProperty with PriorityEvent
   case class Orange(careContactId: String, timestamp: String) extends PatientProperty with PriorityEvent
@@ -16,11 +17,17 @@ package API_PatientEvent {
   sealed trait AttendedEvent
   case class Attended(careContactId: String, timestamp: String, attended: Boolean, doctorId: String) extends PatientProperty with AttendedEvent
 
+  sealed trait ExaminationEvent
+  case class Examination(careContactId: String, timestamp: String, isOnExam: Boolean) extends PatientProperty with ExaminationEvent
+
   sealed trait LocationEvent
   case class RoomNr(careContactId: String, timestamp: String, roomNr: String) extends PatientProperty with LocationEvent
 
   sealed trait TeamEvent
   case class Team(careContactId: String, timestamp: String, team: String, klinik: String) extends PatientProperty with TeamEvent
+
+  sealed trait ReasonForVisitEvent
+  case class ReasonForVisit(careContactId: String, timestamp: String, reason: String) extends PatientProperty with ReasonForVisitEvent
 
   sealed trait LatestEventEvent
   case class LatestEvent(careContactId: String, timestamp: String, latestEvent: String, timeDiff: Long) extends PatientProperty with LatestEventEvent
