c6d696707d0dfe2bb03380a654b59676595a31af
Edvard Lindelöf <edvard@Bulldozer2.BulldozerHome>
2017-06-06 16:26:01 +0200

    cleaned up the ResponsiveReactGridLayout facade

--- gui/src/main/scala/spgui/dashboard/Dashboard.scala
+++ gui/src/main/scala/spgui/dashboard/Dashboard.scala
@@ -12,6 +12,8 @@ import spgui.SPWidgetBase
 import spgui.circuit._
 import spgui.WidgetList
 
+import spgui.dashboard.{ ResponsiveReactGridLayout => RRGL }
+
 object Dashboard {
   case class Props(proxy: ModelProxy[(Map[UUID, OpenWidget], GlobalState)])
 
@@ -24,33 +26,34 @@ object Dashboard {
 
         val frontEndState = p.proxy()._2
 
-        ReactGridLayoutItem(
-          key = openWidget.id.toString,
+        val layoutElement = RRGL.LayoutElement(
           i = openWidget.id.toString,
           x = openWidget.layout.x,
           y = openWidget.layout.y,
           w = openWidget.layout.w,
           h = openWidget.layout.h,
           isDraggable = true,
-          isResizable = true,
-          child = DashboardItem(
+          isResizable = true
+        )
+
+        <.div(
+          DashboardItem(
             WidgetList.map(openWidget.widgetType)._1(
-              SPWidgetBase(
-                openWidget.id,
-                frontEndState
-              )
+              SPWidgetBase(openWidget.id, frontEndState)
             ),
             openWidget.widgetType,
             openWidget.id
-          )
+          ),
+          ^.key := openWidget.id.toString,
+          VdomAttr("data-grid") := layoutElement
         )
       }
 
-      val rg = ReactGridLayout(
+      val rg = RRGL(
         width = 1920,
         draggableHandle = "." + DashboardCSS.widgetPanelHeader.htmlClass,
         onLayoutChange = layout => {
-          layout.asInstanceOf[LayoutData].foreach(
+          layout.asInstanceOf[RRGL.Layout].foreach(
             g => {
               p.proxy()._1.values.toList.foreach(widget => if (widget.id.toString == g.i) {
                 val newLayout = WidgetLayout(g.x, g.y, g.w, g.h)
