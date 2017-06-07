e0e9ad0ccbbb6f94de03ce1713f9e3a06c40e3f6
Martin Dahl <martin.dahl@gmail.com>
2017-06-07 15:45:45 +0200

    Port new domain to spgui

--- gui/src/main/scala/spgui/widgets/abilityhandler/APIs.scala
+++ gui/src/main/scala/spgui/widgets/abilityhandler/APIs.scala
@@ -68,10 +68,10 @@ package APIAbilityHandler {
 
   case class Ability(name: String,
                      id: ID,
-                     preCondition: PropositionCondition = PropositionCondition(AlwaysFalse, List()),
-                     started: PropositionCondition = PropositionCondition(AlwaysFalse, List()),
-                     postCondition: PropositionCondition = PropositionCondition(AlwaysTrue, List()),
-                     resetCondition: PropositionCondition = PropositionCondition(AlwaysTrue, List()),
+                     preCondition: Condition = Condition(AlwaysFalse, List()),
+                     started: Condition = Condition(AlwaysFalse, List()),
+                     postCondition: Condition = Condition(AlwaysTrue, List()),
+                     resetCondition: Condition = Condition(AlwaysTrue, List()),
                      parameters: List[ID] = List(),
                      result: List[ID] = List(),
                      attributes: SPAttributes = SPAttributes())
