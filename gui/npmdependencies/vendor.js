fa902906ce1d5e6c815b111dbc91f70957322db9
aleastChs <aleast@student.chalmers.se>
2017-08-25 13:08:04 +0200

    Added script to vendor.js

--- gui/npmdependencies/vendor.js
+++ gui/npmdependencies/vendor.js
@@ -16,6 +16,16 @@ require('jsoneditor/dist/jsoneditor.css');
 var jQuery = require('jquery');
 window.jQuery = jQuery;
 
+window.GoogleChartsLoaded = false;
+const script = (typeof window !== 'undefined') ? require('scriptjs') : null;
+script("https://www.gstatic.com/charts/loader.js", () => {
+    google.charts.load('current', {packages: ['corechart','gantt','timeline']});
+    google.charts.setOnLoadCallback(() => {
+        window.GoogleChartsLoaded = true;
+        window.GoogleVisualization = google.visualization;
+    });
+});
+
 var bootstrap = require('bootstrap');
 window.bootstrap = bootstrap;
 require('bootstrap/dist/css/bootstrap.css');
