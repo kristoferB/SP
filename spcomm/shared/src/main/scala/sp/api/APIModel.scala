6037671de6659e91c085092264e37c72804d21bc
Kristofer Bengtsson <kristofer@sekvensa.se>
2017-09-12 17:07:29 +0300

    Updated model api

--- spcomm/shared/src/main/scala/sp/api/APIModel.scala
+++ spcomm/shared/src/main/scala/sp/api/APIModel.scala
@@ -2,24 +2,46 @@ package sp.models {
 
 import sp.domain._
 import Logic._
-import sp.domain.SchemaLogic._
 
-import scala.util.Try
+object APIModelMaker {
+  sealed trait Request
+  sealed trait Response
+  val service = "ModelMaker"
 
+  case class CreateModel(name: String, attributes: SPAttributes = SPAttributes(), id: ID = ID.newID) extends Request
+  case class DeleteModel(id: ID) extends Request
+  case object GetModels extends Request
+
+  case class ModelCreated(name: String, attributes: SPAttributes = SPAttributes(), model: ID) extends Response
+  case class ModelDeleted(model: ID) extends Response
+
+  object Formats {
+    import play.api.libs.json._
+
+    implicit val fCreateModel: JSFormat[CreateModel] = Json.format[CreateModel]
+    implicit val fDeleteModel: JSFormat[DeleteModel] = Json.format[DeleteModel]
+    implicit val fGetModels : JSFormat[GetModels.type] = deriveCaseObject[GetModels.type]
+    implicit val fModelDeleted: JSFormat[ModelDeleted] = Json.format[ModelDeleted]
+    def defModelMakerRequest: JSFormat[Request] = Json.format[Request]
+    def defModelMakerResponse: JSFormat[Response] = Json.format[Response]
+  }
+
+  object Request {
+    implicit lazy val fModelMakerRequest: JSFormat[Request] = Formats.defModelMakerRequest
+  }
+  object Response {
+    implicit lazy val fModelMakerResponse: JSFormat[Response] = Formats.defModelMakerResponse
+  }
+
+}
 
 object APIModel {
   sealed trait Request
   sealed trait Response
-  val service = "Model"
+  val service = "AModel"
 
-  case class CreateModel(name: String, attributes: SPAttributes = SPAttributes(), id: ID = ID.newID) extends Request
-  case class DeleteModel(id: ID) extends Request
-  case class UpdateModelAttributes(name: Option[String], attributes: Option[SPAttributes]) extends Request
-  case class RevertModel(toVersion: Int) extends Request
-  case class Import(name: String, id: ID, version: Int, attributes: SPAttributes = SPAttributes(), items: List[IDAble]) extends Request
 
   case object GetModel extends Request
-  case object GetModels extends Request
   case object GetModelInfo extends Request
   case object GetModelHistory extends Request
 
@@ -27,10 +49,14 @@ object APIModel {
   case class GetItem(itemID: ID) extends Request
   case class GetItemsInList(items: List[ID]) extends Request
   case object GetStructures extends Request
-  // add more messages here later
+
 
   case class PutItems(items: List[IDAble], info: SPAttributes = SPAttributes()) extends Request
   case class DeleteItems(items: List[ID], info: SPAttributes = SPAttributes()) extends Request
+
+  case class UpdateModelAttributes(name: Option[String], attributes: Option[SPAttributes]) extends Request
+  case class RevertModel(toVersion: Int) extends Request
+  case class Import(name: String, id: ID, version: Int, attributes: SPAttributes = SPAttributes(), items: List[IDAble]) extends Request
 
   case class TheModel(name: String, id: ID, version: Int, attributes: SPAttributes = SPAttributes(), items: List[IDAble]) extends Response
   case class ModelInformation(name: String, id: ID, version: Int, attributes: SPAttributes = SPAttributes()) extends Response
@@ -44,13 +70,10 @@ object APIModel {
   object Formats {
     import play.api.libs.json._
 
-    implicit val fCreateModel: JSFormat[CreateModel] = Json.format[CreateModel]
-    implicit val fDeleteModel: JSFormat[DeleteModel] = Json.format[DeleteModel]
     implicit val fUpdateModelAttributes: JSFormat[UpdateModelAttributes] = Json.format[UpdateModelAttributes]
     implicit val fRevertModel: JSFormat[RevertModel] = Json.format[RevertModel]
     implicit val fImport: JSFormat[Import] = Json.format[Import]
     implicit val fGetModel : JSFormat[GetModel.type] = deriveCaseObject[GetModel.type]
-    implicit val fGetModels : JSFormat[GetModels.type] = deriveCaseObject[GetModels.type]
     implicit val fGetModelInfo : JSFormat[GetModelInfo.type] = deriveCaseObject[GetModelInfo.type]
     implicit val fGetModelHistory : JSFormat[GetModelHistory.type] = deriveCaseObject[GetModelHistory.type]
     implicit val fGetItems : JSFormat[GetItems.type] = deriveCaseObject[GetItems.type]
@@ -80,6 +103,7 @@ object APIModel {
 
 }
 
-
-
 }
+
+
+
