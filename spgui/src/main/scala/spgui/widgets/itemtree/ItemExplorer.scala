4c8fb44589da6623354feb55866dac26a3b0cdb3
Edvard <edvardlindelof@gmail.com>
2017-09-28 11:09:57 +0200

    itemtree: opens list of items of currentModel

--- spgui/src/main/scala/spgui/widgets/itemtree/ItemExplorer.scala
+++ spgui/src/main/scala/spgui/widgets/itemtree/ItemExplorer.scala
@@ -8,49 +8,87 @@ import spgui.{SPWidget, SPWidgetBase}
 import spgui.components.DragAndDrop.DataOnDrag
 import spgui.communication.BackendCommunication
 import spgui.widgets.itemeditor.{API_ItemServiceDummy => api}
+import spgui.circuit.{ SPGUICircuit, UpdateGlobalState, GlobalState }
 import sp.domain._
 
 import scalajs.js
 import js.Dynamic.{literal => l}
 import js.JSON
+import js.annotation.JSExportTopLevel
 import java.util.UUID
 
 
 object ItemExplorer {
 
-  case class ItemExplorerState(items: List[IDAble])
+  // TODO temporary way of setting currentModel
+  @JSExportTopLevel("setCurrentModel")
+  def setCurrentModel(modelIDString: String) = {
+    val id = UUID.fromString(modelIDString)
+    val action = UpdateGlobalState(GlobalState(currentModel = Some(id)))
+    SPGUICircuit.dispatch(action)
+  }
+
+  import sp.models.{APIModel => mapi}
+
+  def extractMResponse(m: SPMessage) = for {
+    h <- m.getHeaderAs[SPHeader]
+    b <- m.getBodyAs[mapi.Response]
+  } yield (h, b)
+
+  def makeMess(h: SPHeader, b: mapi.Request) = SPMessage.make[SPHeader, mapi.Request](h, b)
+
+  case class ItemExplorerState(items: List[IDAble], modelIDFieldString: String = "modelID")
 
   class ItemExplorerBackend($: BackendScope[SPWidgetBase, ItemExplorerState]) {
 
-    val messObs = BackendCommunication.getMessageObserver(
-      _.getBodyAs[api].foreach{
-        case items: api.SampleItemList => {
-          println(items)
-          $.setState(ItemExplorerState(items.items)).runNow()
+    def handleMess(mess: SPMessage): Unit = {
+      println("handlemess: " + mess)
+      extractMResponse(mess).map{ case (h, b) =>
+        val res = b match {
+          case tm@mapi.SPItems(items) => $.modState(s => s.copy(items = items))
+          case x => Callback.empty
         }
-      },
-      "itemExplorerAnswers"
-    )
-
-    def sendCommand(cmd: api) = Callback{
-      val h = SPHeader(from = "ItemExplorerWidget", to = api.attributes.service)
-      val jsonMsg = SPMessage.make(h, cmd)
-      BackendCommunication.publishMessage("services", jsonMsg)
+        res.runNow()
+      }
     }
 
+    def sendToModel(model: ID, mess: mapi.Request): Callback = {
+      val h = SPHeader(from = "ItemExplorer", to = model.toString,
+        reply = SPValue("ItemExplorer"))
+      val json = makeMess(h, mess)
+      BackendCommunication.publish(json, mapi.topicRequest)
+      Callback.empty
+    }
+
+    val topic = mapi.topicResponse
+    val wsObs = BackendCommunication.getWebSocketStatusObserver(  mess => {
+      if (mess) $.props.map(p => p.frontEndState.currentModel.foreach(m => sendToModel(m, mapi.GetItemList()))).runNow()
+    }, topic)
+    val topicHandler = BackendCommunication.getMessageObserver(handleMess, topic)
+
     def render(p: SPWidgetBase, s: ItemExplorerState) =
+      if (p.frontEndState.currentModel.isDefined) renderItems(s.items) else renderIfNoModel
+
+    def renderIfNoModel =
       <.div(
-        <.button("fetch items from backend", ^.onClick --> sendCommand(api.RequestSampleItems())),
+        "No model selected. Create a model with som items in ModelsWidget and call setCurrentModel(idString) in console to select one, then open this widget again"
+      )
+
+    def renderItems(items: List[IDAble]) =
+      <.div(
         <.ul(
-          s.items.toTagMod(idAble => <.li(idAble.name, DataOnDrag(idAble.id.toString)))
+          items.toTagMod(idAble => <.li(idAble.name, DataOnDrag(idAble.id.toString)))
         )
       )
   }
 
-  val ItemExplorerComponent = ScalaComponent.builder[SPWidgetBase]("ItemExplorer")
+  val itemExplorerComponent = ScalaComponent.builder[SPWidgetBase]("ItemExplorer")
     .initialState(ItemExplorerState(Nil))
     .renderBackend[ItemExplorerBackend]
     .build
 
-  def apply() = SPWidget(spwb => ItemExplorerComponent(spwb))
+  def apply() = SPWidget { spwb =>
+    //println(spwb.frontEndState)
+    itemExplorerComponent(spwb)
+  }
 }
